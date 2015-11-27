eec777e6dbbe4249698ada6a126cfaa9c5359e05
Daniil <adiymr@mail.ru>
2015-11-27 19:37:51 +0300

    Update Lab5

--- change_duty_in_delivery.sql
+++ change_duty_in_delivery.sql
@@ -5,13 +5,12 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `change_duty_in_delivery`(IN `change
 
 BEGIN
 
-IF changed_good_ID IN (SELECT goods.id_good FROM goods, ratio_goods_delivery, delivery WHERE 
-								(delivery.id_delivery = ratio_goods_delivery.delivery_id AND ratio_goods_delivery.GD_good_id = changed_good_ID)) THEN
-			BEGIN
-				UPDATE delivery SET delivery.deliverys_duty = delivery.deliverys_duty + addition;
-			END;
-END IF;
+UPDATE delivery SET delivery.deliverys_duty = delivery.deliverys_duty + addition
+#Вложенный запрос IN (SELECT T FROM (SELECT )AS T ) - для защиты от error 1093
+WHERE delivery.del_status = 'wait' AND delivery.id_delivery IN(SELECT T.id_delivery 
+FROM(SELECT delivery.id_delivery FROM goods, ratio_goods_delivery, delivery WHERE 
+(delivery.id_delivery = ratio_goods_delivery.delivery_id AND ratio_goods_delivery.GD_good_id = changed_good_ID)) AS T);
 
 END//
-
+ 
 delimiter ;

942462d90802437b2b76a1190b2a3d2e47162475
Daniil <adiymr@mail.ru>
2015-11-27 16:50:49 +0300

    Remade Lab4

--- carriers_success.sql
+++ carriers_success.sql
@@ -1,15 +1,31 @@
 delimiter //
 
+DROP PROCEDURE IF EXISTS `carriers_success`//
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `carriers_success`(IN `max_value` INT(11), IN `date_1` DATE, IN `date_2` DATE)
 BEGIN
 
-SELECT carrier, SUM(delivery_total_weight) AS DTS
+SET @date_diff = (SELECT DATEDIFF(date_1, date_2))-1; #Сначала мЕньшая дата, потом большая - нам нужно отр. число
+SET @date_3 = DATE_ADD(date_1, INTERVAL @date_diff DAY);
+SET @date_4 = DATE_ADD(date_1, INTERVAL -1 DAY);
+
+SELECT date_1, date_2, T1.*, @date_3, @date_4, T2.*, T1.DTS_1-T2.DTS_2 AS success FROM 
+
+(SELECT carrier, SUM(delivery_total_weight) AS DTS_1
 FROM delivery
 WHERE (DATE(delivery_time) BETWEEN date_1 AND date_2)
 GROUP BY carrier
-HAVING DTS>max_value
-ORDER BY carrier DESC;
+ORDER BY carrier DESC)T1,
 
+(SELECT carrier, SUM(delivery_total_weight) AS DTS_2
+FROM delivery
+WHERE (DATE(delivery_time) BETWEEN @date_3 AND @date_4)
+GROUP BY carrier
+ORDER BY carrier DESC)T2
+
+WHERE T1.carrier=T2.carrier
+HAVING success>=max_value
+ORDER BY T1.carrier DESC;
 
 END//
 
--- dynamic_transit.sql
+++ dynamic_transit.sql
@@ -1,16 +1,31 @@
 delimiter //
-
+DROP PROCEDURE IF EXISTS `dynamic_transit`//
 CREATE DEFINER=`root`@`localhost` PROCEDURE `dynamic_transit`(IN `chk_date` DATE)
 BEGIN
 
-SELECT goods.good_name, count(goods.good_name) AS number_of_goods, QUARTER(delivery.delivery_time)
+SELECT T1.good_name, T1.Checked_year, T1.number_of_goods, T1.Checked_quarter,
+T2.good_name, T2.Previous_year, T2.number_of_goods, T2.Checked_quarter
+FROM
+
+(SELECT goods.good_name, count(goods.good_name) AS number_of_goods, YEAR(delivery.delivery_time) AS Checked_year, QUARTER(delivery.delivery_time) AS Checked_quarter
 FROM goods, ratio_goods_delivery, delivery
 #Для начала - не забываем про связь многие-ко-многим.
 WHERE goods.id_good=ratio_goods_delivery.GD_good_id AND ratio_goods_delivery.delivery_id=delivery.id_delivery
 #Потом - задаём ограничения: год д.б. между указанным в параметре и годом ранее.
-AND (YEAR(delivery.delivery_time)<=YEAR(chk_date) AND YEAR(delivery.delivery_time)>=(YEAR(chk_date)-1))
+AND (YEAR(delivery.delivery_time)<=YEAR(chk_date) AND YEAR(delivery.delivery_time)>(YEAR(chk_date)-1))
 GROUP BY goods.good_name, QUARTER(delivery.delivery_time)
-ORDER BY goods.good_name, QUARTER(delivery.delivery_time); 
+ORDER BY goods.good_name, QUARTER(delivery.delivery_time))T1,
+
+(SELECT goods.good_name, count(goods.good_name) AS number_of_goods, YEAR(delivery.delivery_time) AS Previous_year, QUARTER(delivery.delivery_time) AS Checked_quarter
+FROM goods, ratio_goods_delivery, delivery
+#Для начала - не забываем про связь многие-ко-многим.
+WHERE goods.id_good=ratio_goods_delivery.GD_good_id AND ratio_goods_delivery.delivery_id=delivery.id_delivery
+#Потом - задаём ограничения: год д.б. между указанным в параметре и годом ранее.
+AND (YEAR(delivery.delivery_time)<=YEAR(chk_date)-1 AND YEAR(delivery.delivery_time)>(YEAR(chk_date)-2))
+GROUP BY goods.good_name, QUARTER(delivery.delivery_time)
+ORDER BY goods.good_name, QUARTER(delivery.delivery_time))T2
+
+WHERE T1.good_name=T2.good_name AND T1.Checked_quarter=T2.Checked_quarter; 
 
 END//
 

57ccd0dc5c1af161f150d7f73f981fdf90c0504b
Daniil <adiymr@mail.ru>
2015-11-27 13:37:52 +0300

    Update Lab3

--- Group_good_by_month.sql
+++ Group_good_by_month.sql
@@ -1,5 +1,12 @@
-SELECT YEAR(delivery.delivery_time), MONTH(delivery.delivery_time), goods.good_volume  
-FROM ratio_goods_delivery, goods, delivery
-WHERE ratio_goods_delivery.GD_good_id = goods.id_good AND ratio_goods_delivery.delivery_id = delivery.id_delivery
-AND goods.good_name = 'Good_7' #Задаём товар, по которому собираем статистику
-ORDER BY YEAR(delivery.delivery_time), MONTH(delivery.delivery_time);
+SELECT goods.good_name, SUM(duty.duty_total_size) FROM duty, ratio_duty_goods, goods,  delivery, ratio_goods_delivery
+#Сначала связываем товар с пошлиной,
+WHERE duty.id_duty = ratio_duty_goods.duty_id AND ratio_duty_goods.good_id = goods.id_good
+#потом - товар с поставкой.
+AND ratio_goods_delivery.GD_good_id = goods.id_good AND ratio_goods_delivery.delivery_id = delivery.id_delivery
+#Задаём время.
+AND (DATE(delivery_time) BETWEEN '2010-01-01' AND '2015-12-30')
+#ЗАГЛУШКА!!! Иначе оооочень долго
+GROUP BY goods.good_name #Группируем по имени
+HAVING SUM(duty.duty_total_size)>0
+ORDER BY SUM(duty.duty_total_size) DESC
+LIMIT 10; #Выводим всего 10
---
+++ made_indices.sql
@@ -0,0 +1,5 @@
+CREATE INDEX delivery_time ON delivery(delivery_time);
+CREATE INDEX duty_id ON ratio_duty_goods(duty_id);
+CREATE INDEX good_id ON ratio_duty_goods(good_id);
+CREATE INDEX GD_good_id ON ratio_goods_delivery(GD_good_id);
+CREATE INDEX delivery_id ON ratio_goods_delivery(delivery_id);
